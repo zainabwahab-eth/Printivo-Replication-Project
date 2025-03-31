034c744ac134aff369484dba57e0a1c4fd1eabd4
Zainab Wahab <zainabwahab.eth@gmail'com>
2025-03-31 11:33:16 +0100

    Minor changes

--- Printivo-app/src/components/Hero.jsx
+++ Printivo-app/src/components/Hero.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import printivoLogo from "../assets/logo.svg";
 import cartIcon from "../assets/cart.svg";
 import searchIcon from "../assets/search.svg";
+import heroImage from "../assets/lp-image.png";
 
 function Hero() {
   return (
     <div className="bg-[#ebe8d9] px-20 py-4 flex flex-col gap-20 pb-24">
       <div className="flex item-center justify-between items-center">
         <img src={printivoLogo} className="w-30" />
-        <div className="flex item-center gap-12 items-center text-sm font-semibold text-[#384b52]">
+        <div className="flex item-center gap-12 items-center text-sm font-semibold text-[#384b52] cursor-pointer">
           <ul className="flex item-center gap-8 items-center">
             <li>All Products</li>
             <li>Become a Reseller</li>
@@ -18,7 +19,7 @@ function Hero() {
           <ul className="flex item-center gap-8 items-center">
             <li className="text-[#d0908e]">Sign in</li>
             <li>Create an Account</li>
-            <img src={cartIcon} />
+            <img src={cartIcon} alt="cart icon" />
           </ul>
         </div>
       </div>
@@ -38,12 +39,15 @@ function Hero() {
               className="bg-[#f7f6f4] rounded-sm p-6 w-lg "
               id="hero-search"
               type="search"
-              placeholder="Search for Business cards"
+              placeholder="Search for Business cards, T-shirts, Mugs etc"
             />
             <button className=" p-2 h-10 w-10 absolute right-2 bottom-5">
               <img src={searchIcon} />
             </button>
           </div>
+        </div>
+        <div className="absolute right-0 bottom-19 w-110">
+          <img src={heroImage} alt="hero image" />
         </div>
       </div>
     </div>
--- Printivo-app/src/components/Nav.jsx
+++ Printivo-app/src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import whatsappLogo from "../assets/whatsapp.svg";
+import nigeriaFlag from "../assets/nigeria-flag.svg";
 
 function Nav() {
   return (
     <nav className="box-border flex bg-[#f7f6f4] text-[#6b7282] px-20 py-2 justify-between items-center text-sm font-medium">
-      <ul className="flex items-center gap-12">
+      <ul className="flex items-center gap-12 cursor-pointer">
         <li>Cost Calculator</li>
         <li>Discover Stores</li>
         <li>Track Orders</li>
@@ -12,10 +13,18 @@ function Nav() {
       <div className="flex items-center gap-12">
         <div>
           <p>Need help? Call:</p>
-          <p className="text-[#53a0ce]">+2347069000083 or +2349035000505</p>
+          <p>
+            <span className="text-[#53a0ce]">+2347069000083</span> or{" "}
+            <span className="text-[#53a0ce]">+2349035000505</span>
+          </p>
         </div>
         <img src={whatsappLogo} />
-        <div className="border rounded-full py-2 px-4 border-[#d7d7d7]">Nigeria</div>
+        <div className="border rounded-full py-2 px-4 border-[#d7d7d7]">
+          <span className="flex center gap-2">
+            {" "}
+            <img src={nigeriaFlag} /> Nigeria
+          </span>
+        </div>
       </div>
     </nav>
   );
